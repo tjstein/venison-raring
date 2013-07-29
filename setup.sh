add5ae52e7f6557d13d5e9d1167b8851718628e8
TJ Stein <thomasjstein@gmail.com>
2013-07-28 18:23:42 -0700

    adding software-properties-common pkg for ppa support

--- setup.sh
+++ setup.sh
@@ -172,7 +172,7 @@ install_base()
   aptitude update > /dev/null 2>&1
   aptitude -y safe-upgrade > /dev/null 2>&1
   aptitude -y full-upgrade > /dev/null 2>&1
-  aptitude -y install dialog curl build-essential python-software-properties git-core htop > /dev/null 2>&1
+  aptitude -y install dialog curl build-essential python-software-properties software-properties-common git-core htop > /dev/null 2>&1
   service saslauthd stop > /dev/null 2>&1
   update-rc.d -f saslauthd remove > /dev/null 2>&1
   echo "done."
